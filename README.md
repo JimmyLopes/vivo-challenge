512e730d3250cb65f83c5af7e3c30721d0c4de4f
Jimmy Botezine <jimmy.botezine@ntendencia.com.br>
2021-01-08 18:58:01 -0300

    feat(Questao 2): Criado handler e exceções customizadas
    Criado classe de exceção customizada BusinessException e ResourceNotFoundException
    
    Criado handler para capturar e tratar exceção, criado objeto customizado de erro.

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Desafio para novos desenvolvedores, linguagem utilizada para a solução: Java 8
 Desenvolvido uma API Rest com Spring Boot.
 
 Dados salvos no banco H2.
+
+Que a força esteja com você!!!
---
+++ src/main/java/br/com/vivo/challenge/backend/exceptions/BusinessException.java
@@ -0,0 +1,14 @@
+package br.com.vivo.challenge.backend.exceptions;
+
+/**
+ * Exceção personalizada para erros que acontecem na regra de negócio
+ */
+public class BusinessException extends RuntimeException {
+
+	public BusinessException(String message) {
+        super(message);
+    }
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
