948ce509015582e1194d78d97dab8f4a796c52fb
Jimmy Botezine <jbotezine@gmail.com>
2021-01-10 19:37:45 -0300

    fix(Questao 3): Ajuste no segundo parametro do repository de heroi

--- src/main/java/br/com/vivo/challenge/backend/repository/HeroiRepository.java
+++ src/main/java/br/com/vivo/challenge/backend/repository/HeroiRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HeroiRepository extends CrudRepository<Heroi, Integer> {
+public interface HeroiRepository extends CrudRepository<Heroi, String> {
 }
