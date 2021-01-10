9a0a0202871bb836be1fa4065282ee8cab586604
Jimmy Botezine <jbotezine@gmail.com>
2021-01-10 19:42:09 -0300

    refactor(Questao 3): Adicionado validacao por anotacao no dto de novo heroi

--- src/main/java/br/com/vivo/challenge/backend/dto/NovoHeroiDto.java
+++ src/main/java/br/com/vivo/challenge/backend/dto/NovoHeroiDto.java
@@ -1,11 +1,14 @@
 package br.com.vivo.challenge.backend.dto;
 
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
 
 public class NovoHeroiDto {
 
+    @NotBlank(message = "{heroi.codigo.vazio}")
     private String codigoHeroi;
+    @NotBlank(message = "{heroi.nome.vazio}")
     private String nomeHeroi;
 
     private List<VoltaDto> voltasHeroi = new ArrayList<>();
